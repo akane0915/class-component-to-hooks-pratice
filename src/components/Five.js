ce1ec7b8110de2f8e4d34a8214a903ca1bff85c3
akane0915 <asialkane@gmail.com>
2020-04-09 14:31:05 -0700

    complete challenge 5

--- src/components/Five.js
+++ src/components/Five.js
@@ -1,24 +1,37 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 import Little from "./Little";
 
-class Five extends Component {
-  state = {
-    showText: true
-  };
-  showLittle = () => {
-    this.setState({ showText: !this.state.showText });
-  };
+// class Five extends Component {
+//   state = {
+//     showText: true
+//   };
+//   showLittle = () => {
+//     this.setState({ showText: !this.state.showText });
+//   };
 
-  render() {
-    return (
-      <div style={{ marginBottom: "50px" }}>
-        <h2>Challenge 5</h2>
-        <h3>Here below lies little text in a box</h3>
-        <button onClick={this.showLittle}>Click to toggle Little</button>
-        {this.state.showText ? <Little /> : ""}
-      </div>
-    );
-  }
+//   render() {
+//     return (
+//       <div style={{ marginBottom: "50px" }}>
+//         <h2>Challenge 5</h2>
+//         <h3>Here below lies little text in a box</h3>
+//         <button onClick={this.showLittle}>Click to toggle Little</button>
+//         {this.state.showText ? <Little /> : ""}
+//       </div>
+//     );
+//   }
+// }
+
+const Five = () => {
+  const [showText, setShowText] = useState(true);
+
+  return (
+    <div style={{ marginBottom: "50px" }}>
+      <h2>Challenge 5</h2>
+      <h3>Here below lies little text in a box</h3>
+      <button onClick={() => setShowText(!showText)}>Click to toggle Little</button>
+      {showText ? <Little /> : ""}
+    </div>
+  );
 }
 
 export default Five;
--- src/components/Little.js
+++ src/components/Little.js
@@ -1,17 +1,31 @@
-import React, { Component } from "react";
+import React, { Component, useEffect } from "react";
 
-class Little extends Component {
-  componentWillUnmount() {
-    alert("Goodbye!!");
-  }
+// class Little extends Component {
+//   componentWillUnmount() {
+//     alert("Goodbye!!");
+//   }
 
-  render() {
-    return (
-      <div style={{ marginBottom: "50px", border: "1px solid black" }}>
-        <h5> Hi I'm Little and its nice to meet you!!!</h5>
-      </div>
-    );
-  }
+//   render() {
+//     return (
+//       <div style={{ marginBottom: "50px", border: "1px solid black" }}>
+//         <h5> Hi I'm Little and its nice to meet you!!!</h5>
+//       </div>
+//     );
+//   }
+// }
+
+const Little = () => {
+  useEffect(() => {
+    return () => {
+      alert("Goodbye");
+    }
+  });
+
+  return (
+    <div style={{ marginBottom: "50px", border: "1px solid black" }}>
+      <h5> Hi I'm Little and its nice to meet you!!!</h5>
+    </div>
+  );
 }
 
 export default Little;
