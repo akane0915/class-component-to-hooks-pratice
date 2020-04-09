295695946d71d35945caebb58f3c9e3bbad5784b
akane0915 <asialkane@gmail.com>
2020-04-09 13:49:21 -0700

    complete challenge 2

--- src/components/Two.js
+++ src/components/Two.js
@@ -1,22 +1,35 @@
-import React, { Component } from "react";
+import React, { Component, useState } from "react";
 
-class Two extends Component {
-  state = {
-    activeUser: "Chris"
-  };
-  changeUser = () => {
-    this.setState({ activeUser: "Bolingo!" });
-  };
+// class Two extends Component {
+//   state = {
+//     activeUser: "Chris"
+//   };
+//   changeUser = () => {
+//     this.setState({ activeUser: "Bolingo!" });
+//   };
 
-  render() {
-    return (
-      <div style={{ marginBottom: "50px" }}>
+//   render() {
+//     return (
+//       <div style={{ marginBottom: "50px" }}>
+//         <h2>Challenge 2</h2>
+//         <p>Active User is: {this.state.activeUser}</p>
+//         <button onClick={this.changeUser}>Change Me!</button>
+//       </div>
+//     );
+//   }
+// }
+
+const Two = () => {
+  const [user, setUser] = useState("Chris");
+
+  return(
+    <div style={{ marginBottom: "50px" }}>
         <h2>Challenge 2</h2>
-        <p>Active User is: {this.state.activeUser}</p>
-        <button onClick={this.changeUser}>Change Me!</button>
+        <p>Active User is: {user}</p>
+        <button onClick={() => setUser("Bolingo")}>Change Me!</button>
       </div>
-    );
-  }
+  )
 }
 
 export default Two;
+
