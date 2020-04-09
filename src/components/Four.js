c8fb817a8bb4eaf816bcb86fab81ba523aaf8419
akane0915 <asialkane@gmail.com>
2020-04-09 14:21:31 -0700

    complete challenge 4

--- src/components/Four.js
+++ src/components/Four.js
@@ -1,24 +1,45 @@
-import React, { Component } from "react";
+import React, { Component, useEffect, useState } from "react";
 
-class Four extends Component {
-  state = {
-    message: "What's happening this week?"
-  };
+// class Four extends Component {
+//   state = {
+//     message: "What's happening this week?"
+//   };
 
-  componentDidMount() {
+//   componentDidMount() {
+//     setTimeout(() => {
+//       this.setState({ message: "I only know it's gon be lit!!" });
+//     }, 5000);
+//   }
+
+//   render() {
+//     return (
+//       <div style={{ marginBottom: "50px" }}>
+//         <h2>Challenge 4</h2>
+//         <p>Status: {this.state.message}</p>
+//       </div>
+//     );
+//   }
+// }
+
+const Four = () => {
+  const [message, setMessage] = useState("What's happening this week?");
+
+  useEffect(() => {
+    updateMessage();
+  });
+
+  function updateMessage() {
     setTimeout(() => {
-      this.setState({ message: "I only know it's gon be lit!!" });
+      setMessage("I only know it's gon be lit!!");
     }, 5000);
   }
 
-  render() {
-    return (
-      <div style={{ marginBottom: "50px" }}>
-        <h2>Challenge 4</h2>
-        <p>Status: {this.state.message}</p>
-      </div>
-    );
-  }
+  return (
+    <div style={{ marginBottom: "50px" }}>
+      <h2>Challenge 4</h2>
+      <p>Status: {message}</p>
+    </div>
+  )
 }
 
 export default Four;
